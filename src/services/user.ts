99e9a5d51e8a2728a8f12506e08872e24b65ddee
cuining <cncc0824@gmail.com>
2021-07-22 22:08:40 +0800

    fix

--- src/services/user.ts
+++ src/services/user.ts
@@ -5,12 +5,14 @@ export async function query(): Promise<any> {
 }
 
 export async function queryCurrent(): Promise<any> {
-  const currentUid = localStorage.getItem('bmd-uid')
-  return request(`/manager/user/${currentUid}`).then(response => {
-    response.data.avatar = 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png'
-    response.data.name = response.data.username
-    return response
-  })
+  const currentUid = localStorage.getItem('bmd-uid');
+  if (!currentUid) return Promise.resolve({});
+  return request(`/manager/user/${currentUid}`).then((response) => {
+    response.data.avatar =
+      'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png';
+    response.data.name = response.data.username;
+    return response;
+  });
 }
 
 export async function queryNotices(): Promise<any> {
